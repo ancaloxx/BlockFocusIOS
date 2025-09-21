737e54ec9bc68f4002b15be9cb1c7affbb91261a
ardidev <ardidev03@gmail.com>
2025-09-21 20:42:04 +0900

    last source code

---
+++ BlockFocusApp/Dashboard/View/CircularGradientView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularGradientView.swift
+//  BlockFocusApp
+//
+//  Created by ancalox on 21/09/25.
+//
+
+import SwiftUI
+
+struct CircularGradientView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularGradientView()
+}
